3a1fc2397356e0f9af7c893f93b1ffd7bb7c7e88
teri0411 <goaheadman@naver.com>
2022-04-05 15:26:45 +0900

    15:26

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -1,10 +1,13 @@
 'use strict'
-
 const fp = require('fastify-plugin')
-
+const mongoURL = process.env.MONGODB_ENDPOINT
+console.log(mongoURL)
 module.exports = fp(async function (fastify, opts) {
+  if (process.env.NODE_ENV === 'test') {
+    return
+  }
   fastify.register(require('fastify-mongodb'), {
     forceClose: true,
-    url: process.env.MONGODB_ENDPOINT
+    url: mongoURL
   })
 })
