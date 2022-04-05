b7e510dae8e6f3ade287f1005e8a10acc52abaa6
teri0411 <goaheadman@naver.com>
2022-04-05 17:42:14 +0900

    17:42

--- plugins/cors.js
+++ plugins/cors.js
@@ -4,6 +4,7 @@ const fp = require('fastify-plugin')
 
 module.exports = fp(async function (fastify, opts) {
   fastify.register(require('fastify-cors'), {
-    origin: true
+    origin: "*",
+    
   })
 })
